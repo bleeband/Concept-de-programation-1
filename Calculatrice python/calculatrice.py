bd287a940854630dd94e40f0c8da35dfd8ef14b9
Marc-André Dufour <MAD@nottrue.test>
2025-12-08 08:28:35 -0500

    creation du fichier calculatrice

---
+++ Calculatrice python/calculatrice.py
@@ -0,0 +1,18 @@
+# $ git init
+# Initialiser un nouveau dépôt
+# $ git status
+# Voir l'état actuel
+# $ git add fichier.py
+# Ajouter un fichier
+# $ git add .
+# Ajouter tous les fichiers
+
+
+# $ git commit -m "message"
+# Créer un commit
+# $ git log
+# Voir l'historique
+# $ git log --oneline
+# Historique condensé
+# $ git diff
+# Voir les modifications
